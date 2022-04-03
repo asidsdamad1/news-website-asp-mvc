15e2b5c64e08d13c486c63e0c1bb3c462d26fd78
hieu <asidsdamad1@gmail.com>
2022-04-03 15:28:11 +0700

    validate user

--- ViewModel/UserListViewModel.cs
+++ ViewModel/UserListViewModel.cs
@@ -11,21 +11,21 @@ namespace NewsWebsite.ViewModel
         public int userid { get; set; }
 
         [Required(ErrorMessage = "Chưa nhập username, max 20 ký tự")]
-        [StringLength(20)]
+        [StringLength(20, MinimumLength = 4)]      
         public string username { get; set; }
 
         [Required(ErrorMessage = "Chưa nhập password")]
-        [StringLength(100)]
+        [StringLength(100, MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string password { get; set; }
         [Required]
-        [StringLength(100)]
+        [StringLength(100, MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Compare("password", ErrorMessage = "Bạn nhập mật khẩu không trùng nhau!")]
         public string repassword { get; set; }
 
         [Required(ErrorMessage = "Chưa nhập fullname")]
-        [StringLength(30)]
+        [StringLength(30, MinimumLength = 3)]
         public string fullname { get; set; }
 
         [StringLength(50)]
